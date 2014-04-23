b083892fa71715f10b92ddb82e4d2e72b183eaad
bikeath1337 <bobby@waywire.com>
2014-04-23 08:59:39 -0400

    Creates AbstractQueueManager and NoopQueueManager.

--- dborutils/queue_manager.py
+++ dborutils/queue_manager.py
@@ -1,7 +1,36 @@
 import MySQLdb
 
 
-class NoodleQueueManager(object):
+class AbstractQueueManager(object):
+
+    def queue_insert(self, n_keys):
+
+        raise NotImplementedException("Subclasses must implement queue_insert()")
+
+    def queue_update(self, n_keys):
+
+        raise NotImplementedException("Subclasses must implement queue_update()")
+
+    def queue_delete(self, n_keys):
+
+        raise NotImplementedException("Subclasses must implement queue_delete()")
+
+
+class NoopQueueManager(object):
+
+    """ A No-op Queue Manager suitable for testing """
+
+    def queue_insert(self, n_keys):
+        pass
+
+    def queue_update(self, n_keys):
+        pass
+
+    def queue_delete(self, n_keys):
+        pass
+
+
+class NoodleQueueManager(AbstractQueueManager):
 
     ACTION_INSERT = 'create'
     ACTION_UPDATE = 'update'
