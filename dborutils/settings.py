5f9849edfe97ce6445bc9fbb7515e774487db83c
Lea <amstree@me.com>
2014-04-28 13:51:08 -0400

    Adds logging setting file

---
+++ dborutils/settings.py
@@ -0,0 +1,30 @@
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s  %(name)s:%(lineno)s  %(levelname)s - %(message)s"
+        }
+    },
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "simple"
+        }
+    },
+
+    "loggers": {
+        "my_module": {
+            "level": "ERROR",
+            "handlers": ["console"],
+            "propagate": "no"
+        }
+    },
+
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"]
+    }
+}
