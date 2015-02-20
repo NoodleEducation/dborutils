fc86ed8feb0068e61d66428fcd6658ce85741013
Marcy Petroccione <mpetroccione@noodle.org>
2015-02-20 15:37:58 -0500

    Add parsing of mongo spec to allow for username and password provided

--- tests/test_dborutils.py
+++ tests/test_dborutils.py
@@ -35,6 +35,42 @@ class TestDborutils(unittest.TestCase):
                          ['mongodb://noodle:pass@127.0.0.1:27017/development', 27017,
                           'development', 'categories'])
 
+    def test_parse_db_argstring_without_user_pass(self):
+        mongo_spec = '127.0.0.1:development'
+        mongo_connection_tuple = NoodleMongoClient.parse_db_argstring(mongo_spec)
+
+        self.assertEqual(list(mongo_connection_tuple),
+                         ['mongodb://127.0.0.1:27017/development', 27017,
+                          'development'])
+
+    def test_parse_db_argstring_with_user_pass(self):
+        mongo_spec = 'noodle:pass@127.0.0.1:development'
+        mongo_connection_tuple = NoodleMongoClient.parse_db_argstring(mongo_spec)
+
+        self.assertEqual(list(mongo_connection_tuple),
+                         ['mongodb://noodle:pass@127.0.0.1:27017/development', 27017,
+                          'development'])
+
+    def test_parse_db_argstring_with_user_pass_localhost(self):
+        mongo_spec = 'noodle:pass@localhost:development'
+        mongo_connection_tuple = NoodleMongoClient.parse_db_argstring(mongo_spec)
+
+        self.assertEqual(list(mongo_connection_tuple),
+                         ['mongodb://noodle:pass@localhost:27017/development', 27017,
+                          'development'])
+
+    def test_parse_db_argstring_with_user_pass_no_host(self):
+        mongo_spec = 'noodle:pass@development'
+
+        with self.assertRaises(Exception):
+            NoodleMongoClient.parse_db_argstring(mongo_spec)
+
+    def test_parse_db_argstring_without_user_pass_no_host(self):
+        mongo_spec = 'development'
+
+        with self.assertRaises(Exception):
+            NoodleMongoClient.parse_db_argstring(mongo_spec)
+
     def tearDown(self):
         pass
 
