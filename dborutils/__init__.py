43c0e1d1b13b5a5b132167bc24d4dae29242db69
Christopher Beland <cbeland@noodle.com>
2015-10-27 14:19:16 -0400

    Sync author, email, and version with setup.py

--- dborutils/__init__.py
+++ dborutils/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Josvic Zammit'
-__email__ = 'jvzammit@gmail.com'
-__version__ = '0.4.3'
+__author__ = 'Noodle'
+__email__ = 'data@noodle.com'
+__version__ = '0.4.4'
