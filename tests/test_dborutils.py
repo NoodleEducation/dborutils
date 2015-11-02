edda828629ad91ffd561113670ba1e900d905a47
Christopher Beland <cbeland@noodle.com>
2015-11-02 14:58:17 -0500

    Restore main() call in test_dborutils.py

--- tests/test_dborutils.py
+++ tests/test_dborutils.py
@@ -7,6 +7,7 @@ Tests for the dborutils repo.
 import re
 from mock import patch
 from unittest import TestCase
+import unittest
 
 from dborutils.mongo_client import NoodleMongoClient
 from dborutils.key_service import NoodleKeyService
@@ -82,3 +83,6 @@ class TestDborutils(TestCase):
         nks = NoodleKeyService()
         test_key = nks.generate_nice_key(prefix="yz")
         self.assertTrue(re.match("yz\w\w\w\w\w\w\w\w\w\w", test_key))
+
+if __name__ == '__main__':
+    unittest.main()
